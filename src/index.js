4121f1d03bc9d0e90065cd5e2c46ae3ee65052cb
emmydapson <iloriemmanuel00@gmail.com>
2024-09-02 09:59:41 +0100

    fixed

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import authRoutes from './routes/authRoute.js';
 import listingRoutes from './routes/listingRoute.js';
 import mapRoutes from './routes/mapRoute.js';
 import categoryRoutes from './routes/categoryRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';  // Import fileURLToPath from 'url' module
 
+// Configure environment variables
 dotenv.config();
+
+// Connect to the database
 connectDB();
 
 const app = express();
@@ -18,6 +23,7 @@ app.use(cors({
   credentials: true, // Enable sending credentials
 }));
 
+// Parse JSON requests
 app.use(express.json());
 
 // Default route for root path
@@ -30,7 +36,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/listings', listingRoutes);
 app.use('/api/maps', mapRoutes);
 app.use('/api/categories', categoryRoutes);
+
+// ES module replacement for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the "uploads" folder
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
